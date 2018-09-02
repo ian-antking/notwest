3e2bcc955c0fa186808f9bcc881f0dc8f09942d6
Ian King <ian.antking@gmail.com>
2018-09-02 18:58:41 +0100

    CurrentAccount.methods tested and passed.

--- __tests__/currentAccount.test.js
+++ __tests__/currentAccount.test.js
@@ -1,6 +1,6 @@
 const CurrentAccount = require('../src/main.js');
 
-describe('Current Account', () => {
+describe('Current Account construction', () => {
     it('Creates a new current account object for a customer', () => {
         johnSmith = new CurrentAccount();
         expect(johnSmith).toEqual(expect.anything());
@@ -12,5 +12,29 @@ describe('Current Account', () => {
     it('Creates a new current account object with a balance of 0 if no initial deposit.', () =>{
         johnsmith = new CurrentAccount();
         expect(johnsmith.balance).toEqual(0);
+    });
+});
+
+describe('Current Account Methods', () => {
+    it('Deposit method adds to account balance', () => {
+        johnsmith = new CurrentAccount();
+        johnsmith.deposit(5000);
+        expect(johnsmith.balance).toEqual(5000);
+    });
+    it('Withdraw method subtracts from the account balance', () => {
+        johnsmith = new CurrentAccount(10000);
+        johnsmith.withdraw(5000);
+        expect(johnsmith.balance).toEqual(5000);
+    });
+    it('checkBalance method returns account.balance', () => {
+        johnsmith = new CurrentAccount(10000);
+        balance = johnsmith.checkBalance();
+        expect(balance).toEqual(balance);
+    });
+    it('account.statement property is a list of transactions on account', () => {
+        johnSmith = new CurrentAccount();
+        johnSmith.deposit(100);
+        johnSmith.withdraw(100);
+        expect(johnSmith.statement).toEqual(['Deposited £100', 'Withdrew £100']);
     });
 });
