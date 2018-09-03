5c5a1653f1cfe08b925c29d2abb1fd8e89907d27
Ian King <ian.antking@gmail.com>
2018-09-03 19:09:02 +0100

    Edited tests to avoid repetition of variable declarations.

--- __tests__/currentAccount.test.js
+++ __tests__/currentAccount.test.js
@@ -1,47 +1,40 @@
 const CurrentAccount = require('../src/main.js');
 
 describe('Current Account construction', () => {
+    const johnSmith = new CurrentAccount();
+    const sarahJaneSmith = new CurrentAccount(500);
     it('Creates a new current account object for a customer', () => {
-        johnSmith = new CurrentAccount();
         expect(johnSmith).toEqual(expect.anything());
     });
     it('Creates a new current account object with a balance equal to the initial deposit.', () => {
-        johnsmith = new CurrentAccount(500);
-        expect(johnsmith.balance).toEqual(500);
+        expect(sarahJaneSmith.balance).toEqual(500);
     });
     it('Creates a new current account object with a balance of 0 if no initial deposit.', () =>{
-        johnsmith = new CurrentAccount();
-        expect(johnsmith.balance).toEqual(0);
+        expect(johnSmith.balance).toEqual(0);
     });
 });
 
 describe('Current Account Methods', () => {
+    const johnSmith = new CurrentAccount();
+    johnSmith.deposit(5000);
     it('Deposit method adds to account balance', () => {
-        johnsmith = new CurrentAccount();
-        johnsmith.deposit(5000);
-        expect(johnsmith.balance).toEqual(5000);
+        expect(johnSmith.balance).toEqual(5000);
     });
     it('Withdraw method subtracts from the account balance', () => {
-        johnsmith = new CurrentAccount(10000);
-        johnsmith.withdraw(5000);
-        expect(johnsmith.balance).toEqual(5000);
+        johnSmith.withdraw(5000);
+        expect(johnSmith.balance).toEqual(0);
     });
     it('checkBalance method returns account.balance', () => {
-        johnsmith = new CurrentAccount(10000);
-         const balance = johnsmith.checkBalance();
-        expect(balance).toEqual(balance);
+        johnSmith.deposit(5000)
+        const balance = johnSmith.checkBalance();
+        expect(balance).toEqual(5000);
     });
+    const expectedStatement = ['Deposited £5000', 'Withdrew £5000', 'Deposited £5000'];
     it('account.statement property is a list of transactions on account', () => {
-        johnSmith = new CurrentAccount();
-        johnSmith.deposit(100);
-        johnSmith.withdraw(100);
-        expect(johnSmith.statement).toEqual(['Deposited £100', 'Withdrew £100']);
+        expect(johnSmith.statement).toEqual(expectedStatement);
     });
     it('viewStatement function returns statement as a list of transactions', () => {
-        johnSmith = new CurrentAccount();
-        johnSmith.deposit(100);
-        johnSmith.withdraw(100);
-        expect(johnSmith.viewStatement()).toEqual(['Deposited £100', 'Withdrew £100']);
+        expect(johnSmith.viewStatement()).toEqual(expectedStatement);
     });
     it('viewStatement function can be filtered by money in or money out', () => {
         johnSmith = new CurrentAccount();
